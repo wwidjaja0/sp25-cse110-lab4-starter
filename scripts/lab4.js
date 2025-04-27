129ba7773c87a91d6fcc5e7be7c78bc40e5b281a
widjaja <wwidjaja0@gmail.com>
2025-04-26 22:40:01 -0700

    prices type check

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -24,6 +24,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+	if (
+		prices.length === 0 ||
+		!Array.isArray(prices) ||
+		typeof discount !== "number"
+	) {
+		return false;
+	}
+
 	const discounted = [];
 	const length = prices.length;
 	let discountedPrice = 0;
